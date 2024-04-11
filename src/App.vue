11b1b7c11483ae9a70bb5582e9ac448a03f55a3a
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-11 15:19:08 -0300

    adding AddTransaction component to App

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import Header from './components/Header.vue'
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 
 export default {
     components: {
       Header,
       Balance,
       IncomeExpenses,
-      TransactionList
+      TransactionList,
+      AddTransaction
     }
 }
 </script>
@@ -20,6 +22,7 @@ export default {
     <Balance/>
     <IncomeExpenses/>
     <TransactionList/>
+    <AddTransaction/>
   </div>
 
 </template>
