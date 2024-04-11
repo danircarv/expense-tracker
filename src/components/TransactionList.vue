7996dcb68627c3c40aaa19621275c3e8a75cbfef
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-11 15:06:12 -0300

    adding template of Balance, Header, IncomeExpenses and TransactionList and importing in App

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>History</h3>
+        <ul id="list" class="list">
+            <li class="minus">
+                Cash <span>-R$400</span>
+                <button class="delete-btn">x</button>
+            </li>
+            <li class="plus">
+                Paycheck <span>R$800</span>
+                <button class="delete-btn">x</button>
+            </li>
+        </ul>
+</template>
