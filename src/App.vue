4d173992128653a5b90f8ebe831d478e2b743f9c
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-11 16:31:32 -0300

    configuring transactions and calculation of Balance

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,28 @@ import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const transactions = ref([
-    {id: 1, text: 'Flower', amount:-10.55},
+    {id: 1, text: 'Flower', amount:-144.55},
     {id: 2, text: 'Salary', amount:299.97},
     {id: 3, text: 'Book', amount:-10},
     {id: 4, text: 'Camera', amount:150}
-])
+]);
+
+const total = computed (() => {
+  return transactions.value.reduce((acc, transaction)=> {
+    return acc + transaction.amount
+  }, 0)
+
+})
 
 </script>
 
 <template>
   <Header/>
   <div class="container">
-    <Balance/>
+    <Balance :total="total"/>
     <IncomeExpenses/>
     <TransactionList :transactions="transactions"/>
     <AddTransaction/>
